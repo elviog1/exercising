69dc30dc3aafb9e273abdd2cbeda26e56c4565f5
Elvio Galeano <elvio.galeano827@gmail.com>
2022-08-12 18:59:17 -0300

    js de la pagina principal

---
+++ assets/main.js
@@ -0,0 +1,14 @@
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '741af78cb0mshf465b0455eafc7fp1341b4jsnb39c36de646e',
+// 		'X-RapidAPI-Host': 'omgvamp-hearthstone-v1.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://omgvamp-hearthstone-v1.p.rapidapi.com/cards', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
+
+  
